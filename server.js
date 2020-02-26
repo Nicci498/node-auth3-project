e62f1b03519497b4e67ad8c585ddbfe638efebc2
Nicole white <you@example.com>
2020-02-26 11:30:51 -1000

    mvp

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const apiRouter = require('./api/apiRouter.js');
+
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(morgan('dev'));
+
+server.use('/api', apiRouter);
+
+server.get("/", (req, res) => {
+    res.send("It's alive!");
+  });
+
+module.exports = server;
+
